89a04e4d0ff21f5d6c9d81389c20c58c9a18090f
val didar singh <95515558+valdidar@users.noreply.github.com>
2023-07-08 19:30:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+var exphbs  = require('express-handlebars');
+
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'static')))  
+app.use('/', require(path.join(__dirname, 'routes/blog')))
+
+app.listen(port, () => {
+  console.log(`blog app listening on port ${port}`)
+})
